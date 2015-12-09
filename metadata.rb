41af3234bdd761868832324dc8b868b5d7fd598e
Ellert van der Vecht <evandervecht@schubergphilis.com>
2015-12-09 13:04:14 +0100

    more stuff

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,9 @@ maintainer_email 'evandervecht@schubergphilis.com'
 license 'All rights reserved'
 description 'Installs/Configures winscp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+recipe 'winscp::default', 'Installs WinSCP'
 version '0.1.0'
+
+supports 'windows'
 
 depends 'windows'
